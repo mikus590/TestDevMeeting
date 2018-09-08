37c5241d997fa19d3f7c0b6a3c8a24247fe878be
mikus590 <s17371@pjwstk.edu.pl>
2018-09-08 11:56:20 +0200

    [#2.2] oraz [#2.3]

--- vue-devmeetings/src/App.vue
+++ vue-devmeetings/src/App.vue
@@ -1,41 +1,81 @@
-<!--10- 1. There's a <template> part -->
 <template>
   <div id="app">
-    <h2>My awesome list</h2>
     <ul>
       <li v-for="p in products" :key="p.id">{{ p.name }}</li>
     </ul>
-    <p v-if="!products.length">No products!</p>
-    <button v-on:click="removeLast()">Remove last item</button>
+    <p v-if="!products.length">Brak produktu!</p>
+
+    <form @submit.prevent="onSubmit()">
+      <input
+        name="product"
+        v-model="newProduct.name"
+        v-validate="'required|min:3'"
+      >
+      <input
+        name="product2"
+        v-model="newProduct2.name"
+        v-validate="'required|min:10'"
+      >
+
+      <button>Add</button>
+      <div v-show="errors.has('product')">
+        {{ errors.first('product') }}
+      </div>
+      <div v-show="errors.has('product2')">
+        {{ errors.first('product2') }}
+      </div>
+    </form>
   </div>
 </template>
 
-<!--21- 2. A <script> part -->
 <script>
-// 4. Now App is not mounted itself, we're just creating a component (more on that later - hold your horses!)
+import uuid from 'uuid/v4';
+import Vue from 'vue';
+import VeeValidate from 'vee-validate';
+
+Vue.use(VeeValidate);
+
 export default {
   name: 'app',
-  //11/ 5. Data can no longer be just an object to prevent accidental shared state
   data() {
     return {
       products: [{
         id: 0,
-        name: 'Coffee'
+        name: 'Kawka',
+        lok: 'Mag01'
       }, {
         id: 1,
-        name: 'Pizza'
-      }]
+        name: 'Pizza',
+        lok: 'Mag02'
+      }],
+      newProduct: {
+        name: ''
+      },
+      newProduct2: {
+        name: ''
+      }
     }
   },
   methods: {
-    removeLast() {
-      this.products.pop();
+    onSubmit() {
+      // 3. On the JS side we need to use yet another injected value called $validator to validate all the fields
+      this.$validator.validateAll().then(result => {
+        if (!result) {
+          return;
+        }
+        this.products.push({
+          id: uuid(),
+          ...this.newProduct
+        });
+        this.newProduct.name = '';
+        // 4/ and reset validation state after adding a product
+        this.$validator.reset();
+      });
     }
   }
 }
 </script>
 
-<!--9- 3. And <style> part -->
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
